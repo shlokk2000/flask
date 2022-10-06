1c313ed6dba4f044958e8ca87768d7a271f3e8aa
Shlokk Moolchandani <shlokk2000@gmail.com>
2022-10-06 11:35:53 +0100

    newestestestestestk

--- app.py
+++ app.py
@@ -42,6 +42,6 @@ def personadd():
 
 
 if __name__ == '__main__':
-    #with app.app_context():
+    with app.app_context():
         #db.create_all()
-    app.run(debug=True)
+        app.run(debug=True)
